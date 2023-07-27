24bbbe7bc710a0be452abdc409c6e75bb6ae8489
Mehmet <124798497+matayurt@users.noreply.github.com>
2023-07-27 04:03:03 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,44 @@ var bulb = document.querySelector(".bulb");
 var bt_on = document.querySelector(".bt-on");
 var bt_off = document.querySelector(".bt-off");
 
+var bt_on_ln = document.querySelectorAll(".bt-on").length;
+var bt_off_ln = document.querySelectorAll(".bt-off").length;
+
+var add_bt = document.querySelector(".add");
+var add_me = document.querySelector(".add-me");
+
+const turn_on = () => {
+  bt_on.addEventListener("click", () => {
+    bulb.src = "img/lightbulb.png";
+  });
+};
+
+const turn_off = () => {
+  bt_off.addEventListener("click", () => {
+    bulb.src = "img/lamp.png";
+  });
+};
+
+for (i = 0; i < bt_on_ln; i++) {
+  document.querySelectorAll(".bt-on")[i].addEventListener("click", () => {
+    turn_on();
+  });
+}
+
+for (i = 0; i < bt_off_ln; i++) {
+  document.querySelectorAll(".bt-off")[i].addEventListener("click", () => {
+    turn_off();
+  });
+}
+
+add_bt.addEventListener("click", () => {
+  let li = document.createElement("li");
+  li.innerHTML =
+    '<div class="light-pack"> <img src="img/lamp.png" alt="" class="bulb" /> <div class="btns"> <button class="bt-on bt">TURN ON</button> <button class="bt-off bt">TURN OFF</button></div></div>';
+  li.classList.add("list");
+  add_me.appendChild(li);
+});
+
 // var clickEvent = 1;
 // bt_on.addEventListener("click", () => {
 //   clickEvent += 1;
@@ -13,11 +51,3 @@ var bt_off = document.querySelector(".bt-off");
 //     bulb.src = "img/118063.png";
 //   }
 // });
-
-bt_on.addEventListener("click", () => {
-  bulb.src = "img/lightbulb.png";
-});
-
-bt_off.addEventListener("click", () => {
-  bulb.src = "img/lamp.png";
-});
